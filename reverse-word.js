a385b7733da687384b8be02b97f32ef8c65d5e19
Nick Shtayn <nickolasshtayn@protonmail.com>
2024-03-23 18:02:36 -0500

    Attempt to reverse string with no helper functions

--- reverse-word.js
+++ reverse-word.js
@@ -1,12 +1,25 @@
+// Pseudocode
+
+// empty word string
+// empty words array
+// empty reversed string
+
+// loop that adds each index of function string to the empty word string until hitting space
+// takes word and adds it into empty words array
+// continues this loop for each word in the function string
+
+// adds word in words array into reversed string in reverse order
+// return new reversed string
+
 const reverseWordOrder = (string) => {
-    return string
-        .split(' ')
-        .reverse()
-        .join(' ')
+    let word = '';
+    let words = [];
+    let reversedString = '';
+
+    for (i = 0; i < string.length; i++) {
+        if (string[i] === ' ') {
+            word += string[i]
+        }
+    }
 }
-
-// Unit test
-
-console.log(reverseWordOrder("hello world") === "world hello") // returns true
-
-console.log(reverseWordOrder("hello") === "hello") // returns true
+console.log(reverseWordOrder('hello world'))
